e43c9beebf6315fac3e9cb26cefeb175651c6949
Lewis Morris <36413348+lewis-morris@users.noreply.github.com>
2025-08-15 10:52:23 +0100

    style(lint): address pylint warnings

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+"""Sphinx configuration for project documentation."""
+
+# pylint: disable=invalid-name
 project = "semverbump"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 templates_path = ["_templates"]
