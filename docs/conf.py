7caebef6e85e9d4621a8d1002982f32f6e34cc97
Lewis Morris <lewis@arched.dev>
2025-08-16 10:32:29 +0100

    bumpwright config

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 
 project = "bumpwright"
-author = "Lewis Morris"
+author = "Lewis Morris (arched.dev)"
 copyright = f"{datetime.now():%Y}, {author}"
 html_title = project
 
@@ -14,7 +14,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
     "sphinx_copybutton",
-    "myst_parser",
+    "sphinx_wagtail_theme"
 ]
 
 templates_path = ["_templates"]
@@ -57,7 +57,7 @@ myst_enable_extensions = [
 
 # -- HTML output -------------------------------------------------------------
 
-html_theme = "furo"
+html_theme = "sphinx_wagtail_theme"
 
 # Code highlighting (light/dark)
 highlight_language = "python3"
@@ -75,49 +75,18 @@ html_css_files = [
     "https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap",
 ]
 
-colors = {
-    "bg0": " #fbf1c7",
-    "bg1": " #ebdbb2",
-    "bg2": " #d5c4a1",
-    "bg3": " #bdae93",
-    "bg4": " #a89984",
-    "gry": " #928374",
-    "fg4": " #7c6f64",
-    "fg3": " #665c54",
-    "fg2": " #504945",
-    "fg1": " #3c3836",
-    "fg0": " #282828",
-    "red": " #cc241d",
-    "red2": " #9d0006",
-    "orange": " #d65d0e",
-    "orange2": " #af3a03",
-    "yellow": " #d79921",
-    "yellow2": " #b57614",
-    "green": " #98971a",
-    "green2": " #79740e",
-    "aqua": " #689d6a",
-    "aqua2": " #427b58",
-    "blue": " #458588",
-    "blue2": " #076678",
-    "purple": " #b16286",
-    "purple2": " #8f3f71",
-}
 
-html_theme_options = {
-    "light_css_variables": {
-        "font-stack": "JetBrains Mono, sans-serif",
-        "font-stack--monospace": "JetBrains Mono, monospace",
-        "color-brand-primary": colors["purple2"],
-        "color-brand-content": colors["blue2"],
-    },
-    "dark_css_variables": {
-        "color-brand-primary": colors["purple"],
-        "color-brand-content": colors["blue"],
-        "color-background-primary": colors["fg1"],
-        "color-background-secondary": colors["fg0"],
-        "color-foreground-primary": colors["bg0"],
-        "color-foreground-secondary": colors["bg1"],
-        "color-highlighted-background": colors["yellow"],
-        "color-highlight-on-target": colors["fg2"],
-    },
-}
+html_theme_options = dict(
+    project_name="bumpwright",
+    logo="logo_bg.png",
+    logo_alt="bumpwright logo",
+    logo_height=70,
+    logo_url="/",
+    logo_width=70,
+    github_url="https://github.com/lewis-morris/bumpwright",
+    footer_links=",".join(
+        [
+            "About Us|https://arched.dev",
+        ]
+    ),
+)
